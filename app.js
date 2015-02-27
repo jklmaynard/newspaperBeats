f7940feb928b4e8850efcabc1e32b3ae3ccc1ab5
James Maynard <jameskirkmaynard@gmail.com>
2015-02-27 10:14:48 -0800

    Write html files for angular front end functionality.

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ newspaperBeats.config(function($stateProvider) {
     templateUrl: "partials/beats.html",
     controller: 'BeatsCtrl'
   });
+  $stateProvider.state('beats.reporters', {
+    url: "/:beatId",
+    templateUrl: "partials/beats.reporters.html",
+    controller: "ReportersCtrl"
+  });
 
 
 
