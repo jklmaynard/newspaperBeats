56e3b1c8b5ee27a12a68388f3c9463cd2d2cdb1c
James Maynard <jameskirkmaynard@gmail.com>
2015-02-27 14:15:05 -0800

    After attempting an awesome editing method, concede defeat on adding objects to an object from an ng-model.

--- controllers/ReportersController.js
+++ controllers/ReportersController.js
@@ -1,7 +1,19 @@
 newspaperBeats.controller('ReportersCtrl', function ReportersCtrl($scope, $stateParams, BeatsFactory, UtilitiesFactory) {
   $scope.beat = UtilitiesFactory.findById(BeatsFactory.beats, $stateParams.beatId)
   $scope.addReporter = function() {
-    $scope.beat.reporters.push({ name: $scope.reporterName });
+    $scope.beat.reporters.push({
+        name: $scope.reporterName,
+        id: $scope.beat.reporters.length +1,
+        title: $scope.reporterTitle,
+        tenure: $scope.reporterTenure,
+        awards: $scope.reporterAwards,
+        });
     $scope.reporterName = null;
+    $scope.reporterTitle = null;
+    $scope.reporterTenure = null;
+    $scope.reporterAwards = null;
+
+    console.log($scope.beat.reporters)
+
   };
 });
