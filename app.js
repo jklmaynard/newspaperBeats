1bf8c0b2fb836bc950b8891165c540ac1945c990
James Maynard <jameskirkmaynard@gmail.com>
2015-02-27 10:50:47 -0800

    Up and running with a bug.

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ newspaperBeats.config(function($stateProvider) {
     templateUrl: "partials/beats.html",
     controller: 'BeatsCtrl'
   });
+  
   $stateProvider.state('beats.reporters', {
     url: "/:beatId",
     templateUrl: "partials/beats.reporters.html",
--- services/UtilitiesFactory.js
+++ services/UtilitiesFactory.js
@@ -4,7 +4,7 @@ newspaperBeats.factory('UtilitiesFactory', function UtilitiesFactory() {
     findById: function(collection, id) {
         for(var i = 0; i < collection.length; i++) {
           if (collection[i].id = id) {
-            return collection[i;]
+            return collection[i]
           }
         }
   return null;
